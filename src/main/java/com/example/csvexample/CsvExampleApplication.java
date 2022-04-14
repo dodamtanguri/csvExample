16c4f9c1503e6ee87109e5a39572772a54d3c330
DESKTOP-P2FGL4R\KPC-N-0030 <sh663746@gmail.com>
2022-04-14 11:10:46 +0900

    init project

---
+++ src/main/java/com/example/csvexample/CsvExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.csvexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvExampleApplication.class, args);
+    }
+
+}
